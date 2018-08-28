9a2be459697e6f631490d1c48ea11a5e3ff57250
sudnonk <sudnonk12@yahoo.co.jp>
2018-08-28 18:01:20 +0900

    fix

--- src/FormError.js
+++ src/FormError.js
@@ -5,13 +5,11 @@ class FormError extends Component {
     //コンストラクタ。props - 親から渡される情報
     constructor(props) {
         super(props);
-
-        this.state = this.props.status;
     }
 
     render() {
         console.log(this.props.status);
-        if (this.state === null) {
+        if (this.props.status === null) {
             return <br/>;
         } else {
             return (
