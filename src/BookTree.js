da549492033087740ef322d9c5b7744eb076446d
sudnonk <sudnonk12@yahoo.co.jp>
2018-08-30 21:34:23 +0900

    debug

--- src/BookTree.js
+++ src/BookTree.js
@@ -31,7 +31,7 @@ class BookTree extends Component {
         fetch("./backend.php").then(function (res) {
             return res.json();
         }).then(function (json) {
-            _self.setState({data: json.message});
+            _self.setState({data: json.message.json()});
         })
             .catch(console.error);
     }
