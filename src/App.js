64da248e0a1411559dd72c42cf9cef54567e75d1
sudnonk <sudnonk12@yahoo.co.jp>
2018-09-06 15:13:21 +0900

    それらしい見た目に

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import logo from './logo.svg';
 import './App.css';
 import BookTree from "./BookTree";
 
@@ -6,7 +7,12 @@ class App extends Component {
 
     render() {
         return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
                 <BookTree/>
+            </div>
         );
     }
 }
